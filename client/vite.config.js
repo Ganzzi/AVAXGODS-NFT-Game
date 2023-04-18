1cfae725de68950792ba1d49c675ef85ec871f1a
imduyan <61102692+imduyan@users.noreply.github.com>
2023-04-18 21:05:40 +0700

    deploying

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/AVAXGODS-NFT-Game/",
+  // base: "/AVAXGODS-NFT-Game/",
   plugins: [react()],
 });
